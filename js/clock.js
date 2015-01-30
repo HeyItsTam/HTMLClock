ada4bb37ac85c2bb9cd9e19cdb69b847e2ae33b3
Tam Nguyen <tnguy173@calpoly.edu>
2015-01-30 00:46:25 -0800

    added a daily forecast and dynamic background that changes color according the temperature

--- js/clock.js
+++ js/clock.js
@@ -1,4 +1,4 @@
-window.onload = getTime;
+window.onload = getTime(), getTemp();
 
 function getTime() {
    var date = new Date();
@@ -7,4 +7,27 @@ function getTime() {
    
    clockElement.innerHTML = time;
    setTimeout(getTime, 1000);
+}
+
+function getTemp() {
+   $.getJSON("https://api.forecast.io/forecast/b6392d0fd315033d4d14799a2fa54882/35.300399,-120.662362?callback=?", function(data) {
+      $("#forecastLabel").html(data.daily.summary);
+      $("#forecastIcon").attr("src", "img/" + data.daily.icon + ".png");
+      
+      var className;
+      var tempMax = data.daily.data[0].temperatureMax;
+      
+      if (tempMax >= 90)
+         className = "hot";
+      else if (tempMax >= 80)
+         className = "warm";
+      else if (tempMax >= 70)
+         className = "nice";
+      else if (tempMax >= 60)
+         className = "chilly";
+      else
+         className = "cold";
+      
+      $("body").addClass(className);
+   });
 }
